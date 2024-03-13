84848c28012ae554b756e42c09049b1bb7dee9b7
Axley <axley01herrera@gmail.com>
2024-03-13 19:23:32 +0000

    updts

--- src/app/dashboard/dashboard.page.ts
+++ src/app/dashboard/dashboard.page.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit , ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Storage } from '@ionic/storage-angular';
-import { IonModal } from '@ionic/angular';
-import { OverlayEventDetail } from '@ionic/core/components';
 import { TranslateService } from '@ngx-translate/core';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { MainServiceService } from '../service/main-service.service';
+import { AlertController } from '@ionic/angular';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-dashboard',
@@ -13,15 +13,20 @@ import { MainServiceService } from '../service/main-service.service';
 })
 export class DashboardPage implements OnInit {
 
+  enviromentApiUrl: string = "";
   platform: string = "";
   isSupported: boolean = false;
   lang: string = "es";
   customerInfo: any = {};
+  upcomingAppointments: any = {};
 
   constructor(
     private storage: Storage,
     private translate: TranslateService,
     private mainService: MainServiceService,
+    private http: HttpClient,
+    private alertController: AlertController,
+    private router: Router,
   ) {
 
     this.translate.addLangs(['en', 'es']);
@@ -39,12 +44,67 @@ export class DashboardPage implements OnInit {
       this.translate.use(this.lang);
     });
 
+    this.mainService.getStorageEnviromentApiUrl().then((url: any) => {
+      if (url != null) {
+        this.enviromentApiUrl = url;
+      } else
+        this.router.navigate(["intro"]);
+    });
+
     this.mainService.getStorageCustomerInfo().then((customerInfo: any) => {
       this.customerInfo = customerInfo;
-      console.log(this.customerInfo);
-    })
+      this.getUpcomingAppointments(customerInfo.id);
+    });
   }
   ngOnInit() {
+  }
+
+  async getUpcomingAppointments(customerID: any) {
+
+    let introAtention: String = "";
+    let introOk: String = "";
+    let not_network_msg: String = "";
+
+    await this.translate.get('intro.atention').subscribe((res: string) => {
+      introAtention = res;
+    });
+
+    await this.translate.get('intro.ok').subscribe((res: string) => {
+      introOk = res;
+    });
+
+    await this.translate.get('global.not_network_msg').subscribe((res: any) => {
+      not_network_msg = res;
+    });
+
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/x-www-form-urlencoded'
+      })
+    };
+
+    const networkStatus = await this.mainService.getNetworkStatus();
+
+    if (networkStatus) {
+      const apiUrl = this.enviromentApiUrl + "Api/getCustomerUpcomingAppointments";
+      const request = new URLSearchParams();
+      request.set('customerID', customerID);
+      await this.http.post(apiUrl, request.toString(), httpOptions).subscribe((resApi: any) => { // Fetch Api
+        if(resApi) {
+          this.upcomingAppointments = resApi.upcomingAppointments;
+          console.log(this.upcomingAppointments);
+        }
+      }, (error) => {
+        alert('An error has ocurred');
+      });
+    } else { // Error network
+      const alert = await this.alertController.create({
+        header: String(introAtention),
+        message: String(not_network_msg),
+        buttons: [String(introOk)],
+      });
+      await alert.present();
+    }
   }
 
 
